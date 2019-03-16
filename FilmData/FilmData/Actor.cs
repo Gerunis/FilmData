3efa1164e63a8093bccbe5c4459bd3f0dd288425
Gerunis <Gerunis@users.noreply.github.com>
2019-03-16 17:46:52 +0500

    Описания

--- FilmData/FilmData/Actor.cs
+++ FilmData/FilmData/Actor.cs
@@ -4,12 +4,30 @@ using System.Text;
 
 namespace FilmData
 {
+    /// <summary>
+    /// Класс актера
+    /// </summary>
     public class Actor
     {
+        /// <summary>
+        /// Имя
+        /// </summary>
         public string FirstName { get; private set; }
+        /// <summary>
+        /// Фамилия
+        /// </summary>
         public string LastName { get; private set; }
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
         public DateTime DOB { get; private set; }
+        /// <summary>
+        /// Работы. Должность и описание
+        /// </summary>
         public Dictionary<Film, Tuple<Position, string>> Works { get; private set; }
+        /// <summary>
+        /// Фото
+        /// </summary>
         public byte[] Foto { get; private set; }
     }
 }
--- FilmData/FilmData/Film.cs
+++ FilmData/FilmData/Film.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 
 namespace FilmData
 {
+    /// <summary>
+    /// Класс фильма
+    /// </summary>
     public class Film
     {
+        /// <summary>
+        /// Список жанров
+        /// </summary>
         public static List<string> FilmsGenres = new List<string>()
         {
             "Drama", "Romance", "Comedy", "Short",
@@ -15,22 +21,51 @@ namespace FilmData
             "Sport", "Reality-TV", "Talk-Show", "Western",
             "News", "Game-Show", "Film-Noir", "Adult"
         };
-
+        /// <summary>
+        /// Список стран 
+        /// </summary>
         public static List<string> FilmsCountries = new List<string>()
         {
 
         };
+        /// <summary>
+        /// Название фильма
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// Жанры фильма
+        /// </summary>
         public List<string> Genres { get; private set; }
+        /// <summary>
+        /// Год выпуск
+        /// </summary>
         public DateTime Year { get; private set; }
+        /// <summary>
+        /// Страны производства
+        /// </summary>
         public List<string> Countries { get; private set; }
+        /// <summary>
+        /// Продолжительность
+        /// </summary>
         public DateTime Duration { get; private set; }
+        /// <summary>
+        /// Возростной рейтинг
+        /// </summary>
         public AgeRating AgeRating { get; private set; }
+        /// <summary>
+        /// Актеры и съемочная команда. Позиция и описание
+        /// </summary>
         public Dictionary<Actor,Tuple<Position, string>> Credits { get; private set; }
+        /// <summary>
+        /// Постер
+        /// </summary>
         public byte[] Poster { get; private set; }
         
     }
 
+    /// <summary>
+    /// Должности
+    /// </summary>
     public enum Position
     {
         Actor,
@@ -43,6 +78,9 @@ namespace FilmData
         Other
     }
 
+    /// <summary>
+    /// Возростные рейтинги
+    /// </summary>
     public enum AgeRating
     {
         PG,
