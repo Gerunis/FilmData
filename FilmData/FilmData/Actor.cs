bac0c15ea4cebc9ed869dae579de935bfd1fbc6a
Gerunis <Gerunis@users.noreply.github.com>
2019-03-16 17:22:12 +0500

    class

--- FilmData/FilmData/Actor.cs
+++ FilmData/FilmData/Actor.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-    using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Text;
 
 namespace FilmData
 {
     public class Actor
     {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime DOB { get; private set; }
+        public Dictionary<Film, Tuple<Position, string>> Works { get; private set; }
+        public byte[] Foto { get; private set; }
     }
 }
--- FilmData/FilmData/Film.cs
+++ FilmData/FilmData/Film.cs
@@ -5,12 +5,49 @@ namespace FilmData
 {
     public class Film
     {
+        public static List<string> FilmsGenres = new List<string>()
+        {
+            "Drama", "Romance", "Comedy", "Short",
+            "Crime", "Mystery", "Thriller", "  Family",
+            "Action", "Fantasy ", "Adventure", "Sci-Fi",
+            "Animation", "History", "Horror", "Music",
+            "War", "Documentary", "Biography", "Musical",
+            "Sport", "Reality-TV", "Talk-Show", "Western",
+            "News", "Game-Show", "Film-Noir", "Adult"
+        };
 
-        public static List<string> FilmGenres = new List<string>() { };
-        public DateTime Year { get;private set; }
+        public static List<string> FilmsCountries = new List<string>()
+        {
+
+        };
         public string Name { get; private set; }
-        public List<string> Ganres { get; private set; }
-        public List<Actor> Actors { get; private set; }
+        public List<string> Genres { get; private set; }
+        public DateTime Year { get; private set; }
+        public List<string> Countries { get; private set; }
+        public DateTime Duration { get; private set; }
+        public AgeRating AgeRating { get; private set; }
+        public Dictionary<Actor,Tuple<Position, string>> Credits { get; private set; }
+        public byte[] Poster { get; private set; }
         
+    }
+
+    public enum Position
+    {
+        Actor,
+        Director,
+        Writer,
+        Producer,
+        Operator,
+        Composer,
+        Editor,
+        Other
+    }
+
+    public enum AgeRating
+    {
+        PG,
+        PG13,
+        R,
+        NC17
     }
 }
