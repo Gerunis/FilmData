f4e1fc2df36f2b5e231a2633b3ef672b2bd7ff76
Gerunis <Gerunis@users.noreply.github.com>
2019-03-16 16:19:25 +0500

    base

---
+++ FilmData/FilmData/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilmData
+{
+    public class Film
+    {
+
+        public static List<string> FilmGenres = new List<string>() { };
+        public DateTime Year { get;private set; }
+        public string Name { get; private set; }
+        public List<string> Ganres { get; private set; }
+        public List<Actor> Actors { get; private set; }
+        
+    }
+}
